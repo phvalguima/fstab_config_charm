3f5147b8c8769f77821387189128113fedcc0166
Pedro <pedro.guimaraes@canonical.com>
2019-11-27 23:10:38 +0100

    Extended metadata.yaml to hold other series as openstack charms and added first upgrade series zaza test

--- tests/basic_deployment.py
+++ tests/basic_deployment.py
@@ -7,7 +7,9 @@ charm does not have any relations or config options to exercise.
 import unittest
 import time
 import zaza.model as model
+import zaza.utilities.generic as series_upgrade_application
 import asyncio
+
 
 CONFIGMAP_TEST_001 = """- filesystem: {}:/srv/nfs
   mountpoint: /srv/test
@@ -15,8 +17,7 @@ CONFIGMAP_TEST_001 = """- filesystem: {}:/srv/nfs
   options: rw,nosuid
 """
 
-CONFIGMAP_TEST_002 = CONFIGMAP_TEST_001 + """
-- filesystem: /srv/test
+CONFIGMAP_TEST_002 = CONFIGMAP_TEST_001 + """- filesystem: /srv/test
   mountpoint: /srv/testbind
   type: none
   options: bind
@@ -65,7 +66,8 @@ class BasicDeployment(unittest.TestCase):
 
     def test_002_nfs_bind(self):
 
-        fstab_config = CONFIGMAP_TEST_002.format(self.nfs_public_address)
+        nfs = model.get_unit_from_name('ubuntu/0')
+        fstab_config = CONFIGMAP_TEST_002.format(nfs.public_address)
         print("Testing on following option:\n{}".format(fstab_config))
         async def await_app_config(name, config):
             return(await model.async_set_application_config(name, config))
@@ -74,7 +76,16 @@ class BasicDeployment(unittest.TestCase):
         fstab_app = loop.run_until_complete(
             await_app_config('fstab-config',
                              {'configmap': fstab_config}))
-
         result = model.run_on_leader('fstab-config',
                                      'sudo touch /srv/testbind/test002')
+        self.assertEqual(result['Code'], '0')
+
+    def test_003_bionic_to_cosmic_upgrade(self):
+
+        series_upgrade_application('fstab-config',
+                                   pause_non_leader_primary=False,
+                                   from_series="bionic",
+                                   to_series="cosmic")
+        result = model.run_on_leader('fstab-config',
+                                     'sudo touch /srv/testbind/test003')
         self.assertEqual(result['Code'], '0')
